2b41047b5599f6090bb0baff3865368b19586368
mpenner77 <111068440+mpenner77@users.noreply.github.com>
2024-01-12 19:17:32 -0300

    Update main Tokens.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, Query, File, UploadFile
-import psycopg2
+
+from fastapi import FastAPI, Query, Depends, HTTPException, Header
 from psycopg2.extras import RealDictCursor
-import pandas as pd
-from io import StringIO
-import time
-from sqlalchemy import create_engine
+from typing import List
+
 app = FastAPI()
 
-
+# Substitua os valores abaixo pelos seus parâmetros de banco de dados
 db_params = {
     "host": "dpg-cl0kfci37rbc73cabd2g-a.oregon-postgres.render.com",
     "port": 5432,
@@ -18,40 +16,44 @@ db_params = {
 
 table_name = "dados_csv"
 
-colunas = ['data_abertura',
- 'situacao_cadastral',
- 'razao_social',
- 'nome_fantasia',
- 'cnpj',
- 'cnpj_mei',
- 'cnpj_raiz',
- 'atividade_principal_codigo',
- 'atividade_principal_descricao',
- 'contato_telefonico_0_completo',
- 'contato_telefonico_0_tipo',
- 'contato_telefonico_0_ddd',
- 'contato_telefonico_0_numero',
- 'contato_email_0_email',
- 'contato_email_0_dominio',
- 'codigo_natureza_juridica',
- 'descricao_natureza_juridica',
- 'logradouro',
- 'numero',
- 'complemento',
- 'bairro',
- 'cep',
- 'municipio',
- 'uf',
- 'capital_social',
- 'ibge_latitude',
- 'ibge_longitude',
- 'ibge_codigo_uf',
- 'ibge_codigo_municipio',
- 'quadro_societario_0_qualificacao',
- 'quadro_societario_0_nome']
+valid_tokens = ['2d3f27b14b4a4a60a33ea218a53fbc6c',
+ '3c79d0a8d6e846d4a7a8dbd60f66c1bd',
+ 'f8e5e41a13e443de9e049f96e79f1f7d',
+ '1b2492f7157b4c179a0cb92e3d6b9ef1',
+ '41ef2a9cb52b4b689ac5bb5b62d34ad2',
+ '4b07f5871a4b4e7c9d57ab548e504d4e',
+ 'a6f77b0cb10a4decae9f9b9a1bf30fbb',
+ '3ff8138fc6304f28ba938ab3e66658e8',
+ '6b2ef0e087cd45be8c1c51864cf00eab',
+ 'c5d37d32bc8046349a073bc4b4510ac0',
+ 'd8fc1939a4d1463698f4c2d65eb9dd5a',
+ 'd271e8ef285944418f8c0e28fc75da4c',
+ '6df2798a0f8e4d9a9f99e5b8d98f994c',
+ 'ee8de2c4e6d84e3cbf16e6564cc35ef5',
+ 'f03e96d144b3404f80c89e3ff047a0cc',
+ '3a8391e8334646239c0e7a6142cf195d',
+ 'abf0ce88a7de4b1d9a2c0a1b3bbbf1ae',
+ '29299d699a174168a739bd35cda92da5',
+ '8894e031be114a76b8a4063302e811a7',
+ '7e7b9b189ad048349b4b52bc4f4c7962',
+ 'aa38d9de7f5b4f7d8d047b4e4e4ecf22',
+ '94361437c74d4a68b3c7d82a83c63502',
+ 'b80020a637144a42aeb640a4d929ad8b',
+ 'beed3c44c9b5405db5bb924ff8b45d5f',
+ '59c66e2f81b746a18c164b2389918bc7',
+ '60181e5b051243de9abf4e7f8cd66d7b',
+ '1b695c0d36d643a097c2e58a34083326',
+ 'a0da16673bbf49bf8ceab14b89b433b0',
+ '30e3cfbe4e524056b72d5881eb8d93f3',
+ '19935d4e6b7547ae8c0d0d9a86b8cc8c']
+
+
+def verify_token(authorization: str = Header(..., description="Token de Autorização")):
+    if authorization not in valid_tokens:
+        raise HTTPException(status_code=401, detail="Token de Autorização Inválido")
 
 @app.get("/data/{data}")
-async def por_data(data: str, page: int = Query(1, description="Número da página")):
+async def por_data(data: str, page: int = Query(1, description="Número da página"), token: List[str] = Depends(verify_token)):
     # Calculate the start and end index based on the page and items per page
     per_page = 500
     start_idx = (page - 1) * per_page
@@ -77,83 +79,3 @@ async def por_data(data: str, page: int = Query(1, description="Número da pági
         return result
     else:
         return {"message": "Nenhum dado encontrado para a data fornecida."}
-
-
-
-@app.post("/upload-csv/")
-async def upload_csv(file: UploadFile):
-
-    try:
-        if not file.filename.endswith(".csv"):
-            return {"message": "O arquivo deve ser um arquivo CSV."}
-
-        # Lê o arquivo CSV e converte-o em um DataFrame do pandas
-        print(time.asctime())
-        print("lendo arquivo.")
-        csv_content = await file.read()
-        df = pd.read_csv(StringIO(csv_content.decode('latin1')), sep=";",  encoding="latin1", decimal=",", dtype = 'str', usecols=colunas)
-
-
-
-        print("arquivo lido com sucesso.")
-        
-        print(time.asctime())
-        print("inserindo as datas")
-        df["data_abertura"] = pd.to_datetime(df["data_abertura"], format='%d/%m/%Y')
-        df['data_yyyy_mm_dd'] = df["data_abertura"].dt.strftime("%Y%m%d")
-        print("datas inseridas com sucesso.")
-        print(time.asctime())
-        # Connect to the PostgreSQL database
-        db_url = f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['database']}"
-        
-        # Crie uma conexão com o banco de dados
-        engine = create_engine(db_url)
-        
-        print("inserindo os dados no banco")
-        df.to_sql(table_name,engine, if_exists='append', index = False)
-        print("dados inseridos com sucesso.")
-        print(time.asctime())
-        #captura os ultimos 30 dias, e elimina duplicada
-        print("obtendo os ultimos 45 dias.")
-        
-        mantem_45_dias = """
-        DELETE FROM dados_csv
-        WHERE data_abertura < current_date - interval '45 days';
-        """
-        conn = psycopg2.connect(**db_params)
-        cur = conn.cursor()
-        cur.execute(mantem_45_dias)
-        conn.commit()
-        cur.close()
-        conn.close()
-        
-        print("os ultimos 45 dias obtidos com sucesso.")
-        print(time.asctime())
-        
-        
-        remove_duplicata = '''
-        DELETE   FROM dados_csv T1
-          USING       dados_csv T2
-        WHERE  T1.ctid    < T2.ctid      
-          AND  T1.cnpj    = T2.cnpj 
-        );
-        '''
-        
-        
-        conn = psycopg2.connect(**db_params)
-        cur = conn.cursor()
-        cur.execute(remove_duplicata)
-        conn.commit()
-        cur.close()
-        conn.close()
-        
-        
-        print("duplicatas removidas com sucesso.")
-        print(time.asctime())
-        
-        print("rotina finalizada com sucesso.")
-
-        return {'message':'upload do csv realizado com sucesso.'}
-
-    except Exception as e:
-        return{'message': 'erro ao subir o arquivo', 'erro': e}
